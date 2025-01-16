d69167c397ae8f42fa4f2b293454f8f6fb0c5cca
Mirko <mirkocaccavone03@gmail.com>
2025-01-16 19:53:15 +0100

    [feat] add object in es. 2

---
+++ js/script2.js
@@ -0,0 +1,22 @@
+// Snack2
+// Creare un array di oggetti di squadre di calcio. 
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. 
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadreCalcio = [
+    
+    { nome: 'Squadra 1', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 2', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 3', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 4', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 5', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 6', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 7', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 8', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 9', puntiFatti: 0, falliSubiti: 0 },
+    { nome: 'Squadra 10', puntiFatti: 0, falliSubiti: 0 }
+];
+
+console.log(squadreCalcio);
